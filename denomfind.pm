64a8e6c73edf23665fe91a1e57713216c9ced2d3
Toshiyuki Shimono <bin4tsv@gmail.com>
2022-10-20 01:01:26 +0900

    0.302とするべく、Changesとdenomfind.pmを加筆。

--- denomfind.pm
+++ denomfind.pm
@@ -1,6 +1,6 @@
 package App::denomfind ;  
-our $VERSION = '0.301' ; 
-our $DATE = '2022-10-20T00:55+09:00' ; 
+our $VERSION = '0.302' ; 
+our $DATE = '2022-10-20T01:00+09:00' ; 
 
 =encoding utf8
 
