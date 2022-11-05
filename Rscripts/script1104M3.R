b0ba41cbedc2310357f69aec9fe730c658489145
Toshiyuki Shimono <bin4tsv@gmail.com>
2022-11-06 01:18:51 +0900

    script1104M3.R : M=7までだったのを14に増加させて、画像を描くようにした。

--- Rscripts/script1104M3.R
+++ Rscripts/script1104M3.R
@@ -1,17 +1,18 @@
 # for m in `seq 1 7` ; do time ./simudenrep -d3 -m$m -s123 -t1e5 > d3M0${m} & ; done
+# for i in {08..22}  ; do ../utility1/midproc -c d3M$i > d3M$i.C ; done
 # for i in `seq 7` ; do awk -vOFS="\t" "BEGIN{if($i==1){print \"m\",\"d\",\"v\"}}NR>1{print $i,NR-1,\$1  }" d3M0$i.C   ; done > d3MM
 
-tmp <- read.delim ( "d3MM") #[101:200,]
+tmp <- read.delim ( "d3MMx") #[101:200,]
 tmp$v <- tmp$v / 1e5
 library(areaplot)
 #areaplot (x=tmp$d[t], y=tmp$v[t], las=1 ,lwd=1, col=rainbow(24)[12], xaxt="n",yaxt="n", xaxs="i", yaxs="i", xlab="",ylab="", add=F)	
 t<-(7*1000-999):(7*1000);
-areaplot (x=tmp$d[t], y=tmp$v[t], las=1 ,lwd=.2, col=rainbow(24)[k+1], xaxt="n",yaxt="n", xaxs="i", yaxs="i", xlab="",ylab="", add=F)
-for (l in 1:7 ){ 
-	k <- 8 - l 
+areaplot (x=tmp$d[t], y=tmp$v[t], las=1 ,lwd=.3, col=rainbow(24)[k+1], xaxt="n",yaxt="n", xaxs="i", yaxs="i", xlab="",ylab="", add=F)
+for (l in 1:14 ){ 
+	k <- 15 - l 
 	t<-(k*1000-999):(k*1000);
 	#if ( k>1){ par(new=T) }
-	areaplot (x=tmp$d[t], y=tmp$v[t], las=1 ,lwd=.2, col=rainbow(24)[k+1], xaxt="n",yaxt="n", xaxs="i", yaxs="i", xlab="",ylab="", add=T)
+	areaplot (x=tmp$d[t], y=tmp$v[t], las=1 ,lwd=.3, col=rainbow(24)[k+1], xaxt="n",yaxt="n", xaxs="i", yaxs="i", xlab="",ylab="", add=T)
 }
 axis(2,0:4/4,c("0%","25%","50%","75%","100%"),las=1,cex.axis=1.4)
 axis(1,c(1,0:4*250),cex.axis=1.4)
@@ -27,10 +28,11 @@ for (k in 1:100) {
  p <- 1 
  for( d in 1:1000) {  p <- p * ( 1- (1e-3*d)^k) ; if ( p < 0.99) { P[k,d] <- p } ; if ( p<1e-4) { break}}   
 }
-for (k in 1:7) { 
+for (k in 1:14) { 
   #points ( 1:1000, P[k,1:1000], cex=0.2,type='l', lty=1 , col=rgb(0,0,0,0.4),lwd=2)
   #points ( 1:1000, P[k,1:1000], cex=0.2,type='l', lty=2 , col=rgb(1,1,1,0.4),lwd=2)
-  points ( 1:1000, P[k,1:1000], cex=0.2,type='l', lty=1 , col=rgb(0,0,1,0.2),lwd=7)
+  points ( 1:1000, P[k,1:1000], cex=0.2,type='l', lty=1 , col=rgb(0,0,1,0.1),lwd=8)
+  points ( 1:1000, P[k,1:1000], cex=0.2,type='l', lty=1 , col=rgb(0,0,0,0.1),lwd=8)
 }	
 
 ---- ここまで 2
