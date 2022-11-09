b2bdc8477b21c061096d5d340a16a6fd3c576660
Toshiyuki Shimono <bin4tsv@gmail.com>
2022-11-09 12:31:20 +0900

    hist1108tue.R 整理(ほとんどリファクタ)。後で使うときのために。

--- Rscripts/hist1108tue.R
+++ Rscripts/hist1108tue.R
@@ -1,10 +1,10 @@
 library( matlab)
 
-for ( i in 1:10) { x <- read.delim(sprintf("d2m%02d",i),F)[,2] ; print ( max( subset ( x ,x <1) ) ) } 
-
-
+#for ( i in 1:10) { x <- read.delim(sprintf("d2m%02d",i),F)[,2] ; print ( max( subset ( x ,x <1) ) ) } # 1未満の最大値を各ファイルについて求める
 logit <- function ( p ) { atanh ( p * 2 - 1 ) * 2  } # log (p/(1-p))
 sigmoid <- function ( z ) { 1 / ( 1 + exp (-z) ) } # { ( tanh ( z / 2 ) + 1 ) / 2 } # 1 / ( 1 + exp (-z) ) 
+
+# 1枚目のヒストグラム
 ax <- c (0.1^(1:3),0.5,1-0.1^(1:5))
 pp <- function(x){ sub("[^0-9]%","%",sub("0+%","%",sprintf("%0.3f%%",x*100)))  }
 draw <- function(v,c,a) { 
@@ -15,8 +15,9 @@ draw <- function(v,c,a) {
 #for ( i in 1:8) { draw( pmax(-7,pmin(12.8,logit(  read.delim(sprintf("d2m%02d",i),F)[,2] ))) , rainbow(8,v=0.8, alpha=0.7)[i], ifelse ( i==1 , F,T )  ) }
 for ( i in 1:8) { draw( pmax(-7,pmin(12.8,logit(  read.delim(sprintf("d2m%02d",i),F)[,2] ))) , topo.colors(9,alpha=1)[9-i+ifelse(i>4,0,1)], ifelse ( i==1 , F,T )  ) }
 pp(0.1)
-? hist
+#"? hist
 
+# 2枚目のヒストグラム
 
 draw <- function(v,c,a) { 	hist(plot=F,v,breaks = c(-8:24,100)) }#c(-40:120/5,100)) }
 
